815acd29038055dd6a6c1e2070219eb7f9ee8c5e
EliasKnudsen <elias.knudsen@gmail.com>
2025-10-07 09:34:49 +0200

    Remove unnecessary html

--- DevQuiz.WebClient/src/components/AppHeader.vue
+++ DevQuiz.WebClient/src/components/AppHeader.vue
@@ -1,13 +1,8 @@
 <template>
-  <div class="relative w-full pt-16 bg-primary">
+  <div class="relative w-full pt-16 mb-16 bg-primary">
     <!-- Centered Logo -->
     <div class="flex items-center justify-center">
-      <img src="@/assets/logo/Logo darkmode.svg" alt="Logo" />
-    </div>
-
-    <!-- Dark Mode Toggle - Positioned to the right -->
-    <div class="absolute top-0 right-4 flex items-center space-x-3 pt-16">
-      <!-- Toggle Switch -->
+      <img src="@/assets/logo/Logo darkmode.svg" width="200" alt="Logo" />
     </div>
   </div>
 </template>
