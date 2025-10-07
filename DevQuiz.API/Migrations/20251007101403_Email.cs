994d72284a9d67b898c29323e4cdc56aeded0237
EliasKnudsen <elias.knudsen@gmail.com>
2025-10-07 13:04:47 +0200

    Migration files for email column

---
+++ DevQuiz.API/Migrations/20251007101403_Email.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevQuiz.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Email : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Participants",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Participants");
+        }
+    }
+}
