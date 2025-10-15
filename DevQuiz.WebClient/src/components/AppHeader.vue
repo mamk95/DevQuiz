89742b08a9254cbe8a68afffc0e181a2ad6205e7
EliasKnudsen <elias.knudsen@gmail.com>
2025-10-15 12:43:43 +0200

    Change devquiz logo to a smaller png version

--- DevQuiz.WebClient/src/components/AppHeader.vue
+++ DevQuiz.WebClient/src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <div class="relative w-full pt-16 mb-16 bg-primary">
     <!-- Centered Logo -->
     <div class="flex items-center justify-center">
-      <img src="@/assets/logo/logo-darkmode.svg" width="200" alt="Logo" />
+      <img src="@/assets/logo/logo-darkmode.png" width="200" alt="Logo" />
     </div>
   </div>
 </template>
