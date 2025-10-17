54b821559da30f28d506a24fffc8ee0c6ff8c246
elisemjoen <elise@mjoen.no>
2025-10-17 10:12:43 +0200

    Change background colour and expand box to ahow all answers

--- DevQuiz.WebClient/src/views/QuizView.vue
+++ DevQuiz.WebClient/src/views/QuizView.vue
@@ -1,8 +1,8 @@
 <template>
     <QuizLoading v-if="quizStore.loading" />
 
-    <div v-else-if="currentQuestion && currentQuestion.prompt" class="max-w-3xl mx-auto p-4 h-full flex flex-col justify-between">
-      <div class="bg-secondary rounded-lg shadow-lg overflow-hidden">
+    <div v-else-if="currentQuestion && currentQuestion.prompt" class="max-w-3xl mx-auto p-4 min-h-screen flex flex-col">
+      <div class="bg-secondary rounded-lg shadow-lg mb-4">
         <!-- Header -->
         <QuizHeader
           :current-index="sessionStore.currentQuestionIndex"
