fcf666d5eaf831ba1e4973ff32d50ea83d2c01c7
elisemjoen <elise@mjoen.no>
2025-10-17 15:44:11 +0200

    reapply html5 required attribute for consistency and safety net

--- DevQuiz.WebClient/src/views/JoinView.vue
+++ DevQuiz.WebClient/src/views/JoinView.vue
@@ -75,6 +75,7 @@
                 id="phone"
                 v-model="phoneDigits"
                 type="tel"
+                required
                 :maxlength="phoneValidation.maxLength"
                 class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none sm:flex-1"
                 :placeholder="phonePlaceholder"
